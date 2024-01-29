5259b2e7c211bf4b44b6f220a1634a8f78491eb1
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-29 11:09:03 +0300

    customuser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser # to customize the user fields
+
 # Create your models here.
+
+# Custom user model
+class CustomUser(AbstractUser):
+    """The custom user model"""
+    user_type_choices = [
+        ('admin', 'Admin'), # for the admin
+        ('normal_user', 'Normal User') # the normal user
+    ]
+    user_type = models.CharField(max_length=12, choices=user_type_choices, default='normal_user')
+    
+    
+
+    def __str__(self):
+        return "{}".format(self.username)
