f8c18ae9935f98f6e6db8814feb1fa935dd174f0
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-29 11:13:52 +0300

    customuser admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CustomUser
+
+# The customuser admin model
+class CustomUserAdmin(admin.ModelAdmin):
+    """Custom user model for the admin site"""
+    list_display = ['username', 'email', 'user_type', 'is_superuser']
+    list_filter = ['user_type', 'is_superuser']
+    search_fields = ['username', 'email', 'user_type', 'is_superuser']
+
+admin.site.register(CustomUser, CustomUserAdmin) # register to the site
