8455d75bb4b8bdfd9021d16cff6a77bc247a4cf6
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-31 12:01:11 +0300

    login url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.UserCreationView, name='register') # register url
+    path('register/', views.UserCreationView, name='register'), # register url
+    path('login/', views.AuthenticationView, name='login') # login url
 ]
