d5e31f0dca2ea5c191aa3f2b8a760bc64cf9c2b0
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-31 12:01:40 +0300

    login form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm # for the user creation
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # for the user creation and login/authentication
 from .models import CustomUser # the model for user creation
 
 # Custom user creation form
@@ -6,4 +6,11 @@ class CustomerUserCreationForm(UserCreationForm):
     """Form that creates/registers a user"""
     class Meta:
         model = CustomUser
-        fields = ['username', 'email']
+        fields = ['username', 'email'] # the password fields are pulled from the parent class, UserCreationForm
+    
+# Custom authentication/login form
+class CustomUserAuthenticationForm(AuthenticationForm):
+    """Form for login/authentication"""
+    class Meta:
+        model = CustomUser
+        fields = ['username']
