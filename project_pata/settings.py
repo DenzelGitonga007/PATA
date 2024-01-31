7e45b3b600caa5182b2d5fee9d3cbe40e23bf159
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-01-31 11:25:46 +0300

    templates dir

--- project_pata/settings.py
+++ project_pata/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'project_pata.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
