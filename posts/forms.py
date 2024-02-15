95173d8ff7d8555902e53dc57c1a52dacb97008f
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-02-15 09:57:54 +0300

    form for the missign perso

---
+++ posts/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from . models import MissingPerson
+
+# Form the fill the missing person details
+class MissingPersonForm(forms.ModelForm):
+    """Form to fill the missing person details"""
+    class Meta:
+        model = MissingPerson
+        exclude = ['created_at']
