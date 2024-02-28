5321961ccf87a1318675e7b97400c23c51e3f719
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-02-28 12:43:16 +0300

    create missing person url

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'posts'
 
 
 urlpatterns = [
-    
+    path('create_missing_person/', views.create_missing_person, name='create_missing_person'), # create_missing_person
 ]
