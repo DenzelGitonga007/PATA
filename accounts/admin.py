92ea736dd4a8d35748ab01f2e55392d57d7016d5
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-04-16 09:10:32 +0300

    admin mg user profiles

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CustomUser
+from .models import CustomUser, UserProfile
 
 # The customuser admin model
 class CustomUserAdmin(admin.ModelAdmin):
@@ -10,4 +10,14 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_filter = ['user_type', 'is_superuser']
     search_fields = ['username', 'email', 'user_type', 'is_superuser']
 
+# User profile
+class UserProfileAdmin(admin.ModelAdmin):
+    """Admin to manage user profiles"""
+    list_display = ['user',]
+    list_filter = ['user']
+    search_fields = ['user']
+
+
+
 admin.site.register(CustomUser, CustomUserAdmin) # register to the site
+admin.site.register(UserProfile, UserProfileAdmin) # register the user profile to the admin site
