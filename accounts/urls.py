59f8f772919f3c0f444e772105778b5b9dd3df57
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-04-16 09:16:18 +0300

    profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.user_creation_view, name='register'), # register url
     path('login/', views.authentication_view, name='login'), # login url
     path('logout/', views.logout_user, name='logout'), # logout user
-    path('', views.home, name='home') # the home page
+    path('', views.home, name='home'), # the home page
+    path('profile/<int:user_id>/', views.profile_view, name='profile'), # view profile
 
 ]
