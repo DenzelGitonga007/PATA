f51e7cec7241994e77c9b88931582a32ee718001
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-04-17 13:07:50 +0300

    admin see following count

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,23 +1,25 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import CustomUser, UserProfile
 
-# The customuser admin model
 class CustomUserAdmin(admin.ModelAdmin):
     """Custom user model for the admin site"""
     list_display = ['username', 'email', 'user_type', 'is_superuser']
     list_filter = ['user_type', 'is_superuser']
     search_fields = ['username', 'email', 'user_type', 'is_superuser']
 
-# User profile
 class UserProfileAdmin(admin.ModelAdmin):
-    """Admin to manage user profiles"""
-    list_display = ['user',]
+    list_display = ['user', 'get_followers_count', 'get_following_count']
     list_filter = ['user']
     search_fields = ['user']
 
+    def get_followers_count(self, obj):
+        return obj.followers.count()
 
+    def get_following_count(self, obj):
+        return obj.following.count()
 
-admin.site.register(CustomUser, CustomUserAdmin) # register to the site
-admin.site.register(UserProfile, UserProfileAdmin) # register the user profile to the admin site
+    get_followers_count.short_description = 'Followers'
+    get_following_count.short_description = 'Following'
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
