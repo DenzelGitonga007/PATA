efca86451bd02c796cebe223dd7ed812142ac11f
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-04-18 12:05:32 +0300

    chats models

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Conversation, Message
 
-# Register your models here.
+
+# Admin manage chats/conversations
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'participants_list', 'last_updated']
+    filter_horizontal = ['participants']
+
+    def participants_list(self, obj):
+        # Display the list of participants as a comma-separated string
+        return ', '.join([str(participant) for participant in obj.participants.all()])
+    participants_list.short_description = 'Participants'
+
+
+    
+admin.site.register(Conversation, ConversationAdmin)
+
+
+# Admin manage messages
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['sender', 'conversation', 'text', 'timestamp']
+    list_filter = ['sender', 'conversation']
+
+admin.site.register(Message, MessageAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+User = get_user_model() # the user from accounts app
+
+class Conversation(models.Model):
+    """The Chats model"""
+    # Many-to-many relationship with users to store participants of the conversation
+    participants = models.ManyToManyField(User, related_name='conversations')
+    
+    # Timestamp to track when the conversation was last updated
+    last_updated = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        # Display the participants' usernames as the representation of the conversation
+        return ', '.join([str(participant) for participant in self.participants.all()])
+
+class Message(models.Model):
+    """The messages in the chats"""
+    # Foreign key to link messages to their respective conversations
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    
+    # Foreign key to indicate the user who sent the message
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    
+    # Text content of the message
+    text = models.TextField()
+    
+    # Timestamp to track when the message was sent
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        # Display the sender's username and the message content
+        return f'{self.sender}: {self.text}'
