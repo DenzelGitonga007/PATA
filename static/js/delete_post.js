db2ac573144c9422f5a2d32b469237db98b56000
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-04-23 18:15:54 +0300

    delete post

---
+++ static/js/delete_post.js
@@ -0,0 +1,8 @@
+// JavaScript to trigger the modal when delete button is clicked
+$(document).ready(function() {
+    $('.delete-post-btn').click(function() {
+        $('#confirmDeleteModal').modal('show');
+    });
+});
+
+
