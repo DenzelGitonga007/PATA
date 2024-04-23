255d9cbe8cb0599fa1c0e02fd178f4b048f8b351
DenzelGitonga007 <gitongadenzel@gmail.com>
2024-04-23 09:37:14 +0300

    liking js

---
+++ static/js/liking.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const likeBtns = document.querySelectorAll(".like-btn");
+
+    likeBtns.forEach(likeBtn => {
+        likeBtn.addEventListener("click", function() {
+            const postId = likeBtn.dataset.postId;
+
+            fetch(`/posts/react/${postId}/`, {
+                method: "POST",
+                headers: {
+                    "X-Requested-With": "XMLHttpRequest",
+                    "X-CSRFToken": csrfToken, // Use the CSRF token variable here
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                // Update the like count dynamically
+                const likeCountElement = likeBtn.nextElementSibling;
+                likeCountElement.innerText = data.likeCount;
+            })
+            .catch(error => {
+                console.error("Error:", error);
+            });
+        });
+    });
+});
